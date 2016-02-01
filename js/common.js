9cfcf3037fb45f6a8e1c3c3581fc7e63c536a8a9
Konstantin Semenov <kvsemenoff@mail.ru>
2016-02-01 11:59:14 +0300

    modifications

--- js/common.js
+++ js/common.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
-    
+$(".single_image").fancybox();
+
+$("a[rel=group]").fancybox({
+  'transitionIn' : 'none',
+  'transitionOut' : 'none',
+  'titlePosition' : 'over',
+  'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+   return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+   }
+});
+
+
 
   $("#owl-demo").owlCarousel({
    
@@ -31,7 +42,7 @@ $(document).ready(function() {
     $(id).fadeIn(500); 
   
   });
-  $('a[name=modal-otzyv]').click(function(e) {
+  $('a[name=modal-zvonok]').click(function(e) {
     e.preventDefault();
     var id = $(this).attr('href');
     var maskHeight = $(document).height();
@@ -41,35 +52,32 @@ $(document).ready(function() {
     var winH = $(window).height();
     var winW = $(window).width();
     posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    $(id).css('top',  posTop+50);
+    $(id).css('top',  posTop+150);
     $(id).css('left', winW/2-$(id).width()/2);
     $('#otzyv-content').html($(this).find('.otzyv-content').html());
     $(id).fadeIn(500); 
   
   });
-  $('.callback').click(function(e) {
-    $('#otzyv-content').html($(this).parent().find('.otzyv-content').html());
- 
-  });
+  
   $('.window2 .close').click(function (e) {
     e.preventDefault();
     $('#mask, .window2').hide();
   }); 
-  $('.ocenka .close').click(function (e) {
+  $('.zvonok .close').click(function (e) {
     e.preventDefault();
-    $('#mask, .ocenka').hide();
+    $('#mask, .zvonok').hide();
   }); 
 
   $('#mask').click(function () {
       $(this).hide();
       $('.window2').hide();
-      $('.ocenka').hide();
+      $('.zvonok').hide();
 
   }); 
 
-  $("#headerform_submit").click(function () {
+  $("#page8_form_submit").click(function () {
       var empty = true;
-    $('.headerform  input[name="tel"]').each(function(o){
+    $('.page8_form  input[name="tel"]').each(function(o){
         if ($(this).val() == ""){empty = false;}
     });
 
@@ -80,7 +88,7 @@ $(document).ready(function() {
       $.ajax({
         type: 'POST',
         url: '/sendmessage.php',
-        data: $('.headerform').serialize(),
+        data: $('.page8_form').serialize(),
         success: function(data) {
         }
       });
@@ -92,9 +100,9 @@ $(document).ready(function() {
     return false;
   });
 
-  $("#page5-form_submit").click(function () {
+  $("#page10_form_submit").click(function () {
       var empty = true;
-    $('.page5-form  input[name="tel"]').each(function(o){
+    $('.page10_form  input[name="tel"]').each(function(o){
         if ($(this).val() == ""){empty = false;}
     });
 
@@ -105,7 +113,7 @@ $(document).ready(function() {
       $.ajax({
         type: 'POST',
         url: '/sendmessage.php',
-        data: $('.page5-form').serialize(),
+        data: $('.page10_form').serialize(),
         success: function(data) {
         }
       });
@@ -117,95 +125,7 @@ $(document).ready(function() {
     return false;
   });
 
-  
-  $("#any-questions_form").click(function () {
-    var empty = true;
-    $('.any-questions  input[name="email"]').each(function(o){
-        if ($(this).val() == ""){empty = false;}
-    });
 
-    if (empty == false){
-      alert("Заполните, пожалуйста, Ваш email");
-    }else{
-      //alert('sssssss');
-      $.ajax({
-        type: 'POST',
-        url: '/sendmessage.php',
-        data: $('.any-questions').serialize(),
-        success: function(data) {
-        }
-      });
-      //$('body,html').animate({scrollTop:0},500); 
-      $('.window3 .close3').trigger('click');
-      $('a[href=#thanks-consult]').trigger('click');
-    }
-    
-    return false;
-  });
-
-  $("#page3_form_submit").click(function () {
-
-    var empty = true;
-    $('.page3_form  input[name="tel"]').each(function(o){
-        if ($(this).val() == ""){empty = false;}
-    });
-
-    if (empty == false){
-      alert("Заполните, пожалуйста, Ваш телефон");
-    }else{
-      //alert('sssssss');
-      $.ajax({
-        type: 'POST',
-        url: '/sendmessage.php',
-        data: $('.page3_form').serialize(),
-        success: function(data) {
-        }
-      });
-      //$('body,html').animate({scrollTop:0},500); 
-      $('.window3 .close3').trigger('click');
-      $('a[href=#thank-spage3_form]').trigger('click');
-    }
-    
-    return false;
-  });
-  
-  $(".button-3").click(function () {
-    $('a[href=#ocenka]').trigger('click');
-    return false;
-  });
-  
-  $("#ocenka_form_submit").click(function () {
-
-    var empty = true;
-    $('.ocenka_form  input[name="tel"]').each(function(o){
-        if ($(this).val() == ""){empty = false;}
-    });
-
-    if (empty == false){
-      alert("Заполните, пожалуйста, Ваш телефон");
-    }else{
-      //alert('sssssss');
-      $.ajax({
-        type: 'POST',
-        url: '/sendmessage.php',
-        data: $('.ocenka_form').serialize(),
-        success: function(data) {
-        }
-      });
-      //$('body,html').animate({scrollTop:0},500); 
-      $('.ocenka').hide();
-      $('a[href=#thank-spage3_form]').trigger('click');
-    }
-    
-    return false;
-  });
-  
-  $(".callback").click(function () {
-    $('a[href=#zvonok]').trigger('click');
-    return false;
-  });
-  
- 
 
   $("#zvonok_form_submit").click(function () {
 
@@ -226,18 +146,47 @@ $(document).ready(function() {
         }
       });
       //$('body,html').animate({scrollTop:0},500); 
-      $('.ocenka').hide();
-      $('a[href=#thank-spage3_form]').trigger('click');
+      $('.zvonok').hide();
+      $('a[href=#thanks]').trigger('click');
     }
     
     return false;
   });
  
-  $(".phone").mask("+7 (999) 999-9999?");
+ $("#zayavka_form_submit").click(function () {
+
+    var empty = true;
+    $('.zayavka_form  input[name="tel"]').each(function(o){
+        if ($(this).val() == ""){empty = false;}
+    });
+
+    if (empty == false){
+      alert("Заполните, пожалуйста, Ваш телефон");
+    }else{
+      //alert('sssssss');
+      $.ajax({
+        type: 'POST',
+        url: '/sendmessage.php',
+        data: $('.zayavka_form').serialize(),
+        success: function(data) {
+        }
+      });
+      //$('body,html').animate({scrollTop:0},500); 
+      $('.zvonok').hide();
+      $('a[href=#thanks]').trigger('click');
+    }
+    
+    return false;
+  });
+  
+
+
 
 
 
 });
+
+
 
 $(document).ready(function(){
     $('a[href^="#"]').click(function(){ 
